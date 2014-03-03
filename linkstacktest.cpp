b093a121122c1f31652ce8cbee9b86a3eec65a3c
scutLaoYi <scutlaoyi@gmail.com>
2014-03-03 11:02:10 +0800

    New function in stack:
        check the next greater element and print them in O(n) time.

--- linkstacktest.cpp
+++ linkstacktest.cpp
@@ -10,10 +10,12 @@
 #define BUF_SIZE 100
 
 void balanceparentheses();
+void nextgreaterelement();
 
 void functions()
 {
     balanceparentheses();
+    nextgreaterelement();
 }
 
 void linkstacktest()
@@ -47,6 +49,8 @@ void linkstacktest()
     }
     assert(stack->isempty());
     printf("\n");
+
+    delete stack;
 
     printf("Testing stack functions...\n");
     functions();
@@ -110,8 +114,53 @@ void balanceparentheses()
             printf("is balance!\n");
         else
             printf("isn't balance!\n");
+        delete stack;
     }
     return ;
 }
+
+void nextgreaterelement()
+{
+    printf("Input the positive int array with elements to list the next greater elements.\n");
+    printf("End with a '-1'.\n");
+
+    int buf[100];
+    int size(0);
+
+    while(true)
+    {
+        scanf("%d", &buf[size]);
+        if(buf[size] == -1)
+            break;
+        ++size;
+    }
+
+    printf("Data:");
+    for(int i = 0; i < size; ++i)
+        printf("%d ", buf[i]);
+    printf("\n");
+
+    LinkStack<int> *stack = new LinkStack<int>();
+    int ptr(1);
+    stack->push(buf[0]);
+
+    int curelement;
+    while(ptr < size)
+    {
+        while(stack->peek(curelement))
+        {
+            if(curelement >= buf[ptr])
+                break;
+            stack->pop();
+            printf("%d -> %d\n", curelement, buf[ptr]);
+        }
+        stack->push(buf[ptr]);
+        ++ptr;
+    }
+
+    delete stack;
+    return;
+}
+
 
 
